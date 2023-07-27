3103137eb8a74e51efc77023f901d034a79693ab
Kaitlyn Coleman <k8lak8@protonmail.com>
2023-07-26 20:53:25 -0400

    Updated styles and structure to match latest live portfolio

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-const header = document.querySelector('header');
-const toggler = document.querySelector('#mobile-toggler');
-const links = document.querySelector('#navigation');
+const header = document.querySelector("#js-header")
+const toggler = document.querySelector("#js-menu-toggle")
+const links = document.querySelector("#js-nav")
 
 let scrollpos = window.scrollY
 
 //SMOOTH SCROLL ANCHOR LINKS
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-	anchor.addEventListener('click', function (e) {
-		e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+	anchor.addEventListener("click", function (e) {
+		e.preventDefault()
 
-		document.querySelector(this.getAttribute('href')).scrollIntoView({
-			behavior: 'smooth'
-		});
-	});
-});
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+			behavior: "smooth",
+		})
+	})
+})
 
 //MOBILE NAV
-toggler.addEventListener('click', function (e) {
-	if (header.classList.contains('open')) {
-		header.classList.remove('open');
-		toggler.classList.remove('is-active');
+toggler.addEventListener("click", function (e) {
+	if (header.classList.contains("js-open")) {
+		header.classList.remove("js-open")
+		toggler.classList.remove("is-active")
 	} else {
-		header.classList.add('open');
-		toggler.classList.add('is-active');
+		header.classList.add("js-open")
+		toggler.classList.add("is-active")
 	}
-});
+})
 
-links.addEventListener('click', function (e) {
-	header.classList.remove('open');
-	toggler.classList.remove('is-active');
-});
+links.addEventListener("click", function (e) {
+	header.classList.remove("js-open")
+	toggler.classList.remove("is-active")
+})
 
 //FIXED HEADER STYLES ON SCROLL
-window.addEventListener('scroll', function () {
-	scrollpos = window.scrollY;
+window.addEventListener("scroll", function () {
+	scrollpos = window.scrollY
 
 	if (scrollpos >= 100) {
-		header.classList.add("scrolled")
+		header.classList.add("js-scrolled")
 	} else {
-		header.classList.remove("scrolled")
+		header.classList.remove("js-scrolled")
 	}
-});
+})
